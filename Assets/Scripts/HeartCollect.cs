2ff5a77483a9f3280dea5f1641dcde1f512366d4
Hein <hein@hein.local>
2019-01-22 14:42:18 +0630

    Git init , .gitignore, RPG game - heartCollect

---
+++ Assets/Scripts/HeartCollect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartCollect : MonoBehaviour
+{
+    public int rotateSpeed;
+    public AudioSource collectSound;
+    public GameObject thisHeart;
+
+    void Update()
+    {
+        rotateSpeed = 2;
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        collectSound.Play();
+        HeartMonitor.healthValue += 1;
+        thisHeart.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/HeartMonitor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartMonitor : MonoBehaviour
+{
+    public static int healthValue;
+    public int internalHealth;
+    public GameObject heart1;
+    public GameObject heart2;
+    public GameObject heart3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthValue = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        internalHealth = healthValue;
+        if(healthValue == 1)
+        {
+            heart1.SetActive(true);
+        }
+        if (healthValue == 2)
+        {
+            heart2.SetActive(true);
+        }
+        if (healthValue == 3)
+        {
+            heart3.SetActive(true);
+        }
+    }
+}
