2d668b41ec997dc17277b11d5a745aee490ef751
Hein <hein@hein.local>
2019-01-23 18:03:41 +0630

    [WIP] Quest Anim

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public static int activeQuestNumber;
+    public int internalQuestNumber;
+
+    private void Update()
+    {
+        internalQuestNumber = activeQuestNumber;
+    }
+}
