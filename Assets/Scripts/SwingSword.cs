e2ef45707618d68b63a8bfd46db242d078d3eb96
Hein <hein@hein.local>
2019-01-28 10:28:23 +0630

    Added : sword animation and attack on mouse click

---
+++ Assets/Scripts/SwingSword.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingSword : MonoBehaviour
+{
+    public GameObject theSword;
+    public int swordStatus;
+       
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && swordStatus == 0)
+        {
+            StartCoroutine(SwingSwordFn()); 
+        }
+    }
+
+    IEnumerator SwingSwordFn()
+    {
+        swordStatus = 1;
+        theSword.GetComponent<Animation>().Play("ElvenSwordAnim");
+        swordStatus = 2;
+        yield return new WaitForSeconds(1.0f);
+        swordStatus = 0;
+    }
+}
